c7d8e89397a875711d91b8c9d82730c23d2c6350
Mark Charles Smith <markcharlessmith@gmail.com>
2023-05-26 18:15:40 -0700

    added resume

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,39 +11,39 @@ const Navbar = () => {
     <div className="mcs__navbar">
       <div className="mcs__navbar-links">
         <div className="mcs__navbar-links_logo">
-          <img src={mcs} alt="logo"/>
+          <img src={mcs} alt="logo" />
         </div>
         <div className="mcs__navbar-links_container">
           <p><a href="#techstack">Techstack</a></p>
           <p><a href="#experience">Experience</a></p>
           <p><a href={resume} download>Resume</a></p>
-          {/* <p><a href="https://www.markcharlessmith.com">Music</a></p> */}
+          <p><a href="https://www.markcharlessmith.com">Music</a></p>
         </div>
-      <div className="mcs__navbar-links_icons">
-        <a href="https://github.com/markcharlessmith" target="_blank" rel="noreferrer">
-          <RiGithubFill size='40' />
+        <div className="mcs__navbar-links_icons">
+          <a href="https://github.com/markcharlessmith" target="_blank" rel="noreferrer">
+            <RiGithubFill size='40' />
           </a>
-        <a href="https://www.linkedin.com/in/mark-charles-smith" target="_blank" rel="noreferrer">
-          <RiLinkedinBoxFill size='40' />
-        </a>
-        <a href="https://open.spotify.com/artist/3ahbNzDONGv1c0cZFO1IA4" target="blank" rel="nonreferrer">
-          <RiSpotifyFill size='40' />
-        </a>
-      </div>
+          <a href="https://www.linkedin.com/in/mark-charles-smith" target="_blank" rel="noreferrer">
+            <RiLinkedinBoxFill size='40' />
+          </a>
+          <a href="https://open.spotify.com/artist/3ahbNzDONGv1c0cZFO1IA4" target="blank" rel="nonreferrer">
+            <RiSpotifyFill size='40' />
+          </a>
+        </div>
       </div>
       <div className="mcs__navbar-menu">
         {toggleMenu
           ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
           : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
         {toggleMenu && (
-        <div className="mcs__navbar-menu_container scale-up-center">
-          <div className="mcs__navbar-menu_container-links">
-          <p><a href="#techstack">Techstack</a></p>
-          <p><a href="#experience">Experience</a></p>
-          <p><a href={resume} download>Resume</a></p>
-          {/* <p><a href="https://www.markcharlessmith.com">Music</a></p> */}
+          <div className="mcs__navbar-menu_container scale-up-center">
+            <div className="mcs__navbar-menu_container-links">
+              <p><a href="#techstack">Techstack</a></p>
+              <p><a href="#experience">Experience</a></p>
+              <p><a href={resume} download>Resume</a></p>
+              <p><a href="https://www.markcharlessmith.com">Music</a></p>
+            </div>
           </div>
-        </div>
         )}
       </div>
     </div>
